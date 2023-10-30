505abc1509dc8de7c256c388092d67af60f4de09
Meng Li <34143965+Meng6@users.noreply.github.com>
2023-10-30 20:56:21 +0000

    Update sdth.jsonld and add the "closed" test demo

--- demo/03-sdth-validation/run.sh
+++ demo/03-sdth-validation/run.sh
@@ -5,11 +5,14 @@ ERROR_ADDED_SDTH_TTL_FILE="data/error_added_sdth.ttl"
 SDTH_SCHEMA_PATH="data/sdth.schema.ttl"
 MAPPINGS_PATH="data/mappings.json"
 
+TEST_JSONLD_FILE="data/test.jsonld"
+TEST_SCHEMA_PATH_NOTCLOSED="data/test_notclosed.schema.ttl"
+
 # ------------------------------------------------------------------------------
 
 bash_cell 'sdth validation' << END_CELL
 
-rdfvr -f ${SDTH_JSONLD_FILE} -s ${SDTH_SCHEMA_PATH} -m ${MAPPINGS_PATH} -o products/sdth -of png
+rdfvr -f ${SDTH_JSONLD_FILE} -s ${SDTH_SCHEMA_PATH} -m ${MAPPINGS_PATH}
 
 END_CELL
 
@@ -25,7 +28,23 @@ END_CELL
 
 bash_cell 'error added sdth validation txt' << END_CELL
 
-rdfvr -f ${ERROR_ADDED_SDTH_TTL_FILE} -ff turtle -s ${SDTH_SCHEMA_PATH} -m ${MAPPINGS_PATH} -o products/error_added_sdth -of txt
+rdfvr -f ${ERROR_ADDED_SDTH_TTL_FILE} -ff turtle -s ${SDTH_SCHEMA_PATH} -m ${MAPPINGS_PATH}
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell 'closed demo' << END_CELL
+
+rdfvr -f ${TEST_JSONLD_FILE} -s ${SDTH_SCHEMA_PATH} -m ${MAPPINGS_PATH}
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell 'not closed demo' << END_CELL
+
+rdfvr -f ${TEST_JSONLD_FILE} -s ${TEST_SCHEMA_PATH_NOTCLOSED} -m ${MAPPINGS_PATH}
 
 END_CELL
 
